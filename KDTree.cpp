c377de41b56689c1e8b0d6289c3a86faae0fad7e
Gabriel Moczalla <gmoczalla@yahoo.de>
2017-10-27 13:57:40 +0200

    KDTree.cpp hinzugefügt und die definitionen hinzugefpgt (der Code fehlt noch)

---
+++ KDTree.cpp
@@ -0,0 +1,11 @@
+#include "KDTree.h"
+
+KDTree::KDTree(std::vector<Point3d>& points)
+{
+	//Code für KD Konstruktion hier rein
+}
+
+std::vector<Point3d> KDTree::abfrage(int laenge, Point3d& point)
+{
+	//Code für die Abfrage der Punkte die innerhalb des Rechtecks liegen hier rein
+}
