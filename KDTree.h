b3d85d1d42e3b98c73ff9ded73aea4c55e15a3d1
Gabriel Moczalla <gmoczalla@yahoo.de>
2017-10-27 13:52:07 +0200

    Konstruktor und abfrage-Methode deklariert.

--- KDTree.h
+++ KDTree.h
@@ -1,1 +1,9 @@
-#pragma once
+#include <vector>
+#include "Point3d.h"
+
+class KDTree
+{
+public:
+	KDTree(std::vector<Point3d>& points);
+	std::vector<Point3d> abfrage(int laenge, Point3d& point);
+};
