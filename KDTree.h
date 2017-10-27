caf9fa2ea60a99bf09fa967fc74eeab2157c56ba
Kenneth Allan <kenneth.allan@st.ovgu.de>
2017-10-27 16:45:48 +0200

    added kdtree

--- KDTree.h
+++ KDTree.h
@@ -4,6 +4,11 @@
 class KDTree
 {
 public:
-	KDTree(std::vector<Point3d>& points);
+	KDTree(std::vector<Point3d>& points, int dim = 0);
 	std::vector<Point3d> abfrage(int laenge, Point3d& point);
+	Point3d value;
+	KDTree* left;
+	KDTree* right;
+protected:
+	// dim: dimension to spit at
 };
